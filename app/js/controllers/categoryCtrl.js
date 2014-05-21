8ddba522390d06f6fd6d4d4a5fae0a308e4e5065
Kyle Olson <kolson@four51.com>
2014-05-21 13:45:29 -0500

    Initial Commit

---
+++ app/js/controllers/categoryCtrl.js
@@ -0,0 +1,37 @@
+four51.app.controller('CategoryCtrl', ['$routeParams', '$sce', '$scope', '$451', 'Category', 'Product', 'Nav',
+function ($routeParams, $sce, $scope, $451, Category, Product, Nav) {
+	$scope.productLoadingIndicator = true;
+	$scope.trusted = function(d){
+		if(d) return $sce.trustAsHtml(d);
+	}
+	Product.search($routeParams.categoryInteropID, null, null, function(products) {
+        $scope.products = products;
+		$scope.productLoadingIndicator = false;
+    });
+    if ($routeParams.categoryInteropID) {
+	    $scope.categoryLoadingIndicator = true;
+        Category.get($routeParams.categoryInteropID, function(cat) {
+            $scope.currentCategory = cat;
+	        $scope.categoryLoadingIndicator = false;
+        });
+    }else if($scope.tree){
+		$scope.currentCategory ={SubCategories:$scope.tree};
+	}
+
+	$scope.$on("treeComplete", function(data){
+		if (!$routeParams.categoryInteropID) {
+			$scope.currentCategory ={SubCategories:$scope.tree};
+		}
+	});
+
+    // panel-nav
+    $scope.navStatus = Nav.status;
+    $scope.toggleNav = Nav.toggle;
+	$scope.$watch('sort', function(s) {
+		if (!s) return;
+		(s.indexOf('Price') > -1) ?
+			$scope.sorter = 'StandardPriceSchedule.PriceBreaks[0].Price' :
+			$scope.sorter = s.replace(' DESC', "");
+		$scope.direction = s.indexOf('DESC') > -1;
+	});
+}]);
